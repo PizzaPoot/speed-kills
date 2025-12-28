4e5b5a4ca5762d7301db714c15280d724f421f6d
PizzaPoot <viira.mihkel@gmail.com>
2025-12-28 08:12:07 +0200

    Added basic dash with cooldown, Started on main menu and settings

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,8 @@ extends Node
 
 @onready var debug_UI_Timer = %debug_timer
 var elapsed_time := 0.0
-var seconds := 1
+var elapsed_seconds := 1
+var DEBUG_MODE := true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -15,13 +16,16 @@ func _process(delta: float) -> void:
 	elapsed_time: %s
 	delta: %s
 	player speed: %s
-	turn_speed: %s" % 
+	turn_speed: %s
+	dash_cooldown: %s" % 
 	[FreezeFrameManager.timer.time_left, 
 	str(elapsed_time), 
 	delta,
 	%Player.SPEED,
-	%Player.turn_speed])
-	if elapsed_time >= seconds:
-		%Player.SPEED += 10
-		seconds += 1
+	%Player.turn_speed,
+	$"../Player/DashCooldown".time_left])
+	if elapsed_time >= elapsed_seconds:
+		elapsed_seconds += 1
+		if not DEBUG_MODE:
+			%Player.SPEED += 10
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,36 @@
 extends CharacterBody2D
 
-var SPEED := 50.0
+var SPEED := 200.0
+var DashSpeed := 1500.0 #TODO scale with speed
 var turn_speed_divider := 20
 var freezeTime = false
-var DEBUG_GRAPHICS = true
-@onready var input_vector = $Input_vector
-@onready var current_vector = $Current_vector
+@onready var input_vector = $Input_vector #debug
+@onready var current_vector = $Current_vector #debug
+@onready var mouse_vector = $Mouse_vector #debug
+@onready var temp_dash_visual = $Temp_dash_visual
+@onready var dash_timer = $DashTimer
+@onready var dash_cooldown = $DashCooldown
 
 var turn_speed : float = clamp(turn_speed_divider/SPEED,0,1)
-var targetDirection: Vector2 = Vector2.RIGHT
-var moveDirection: Vector2 = Vector2.RIGHT
+var targetDirection := Vector2.RIGHT
+var moveDirection := Vector2.RIGHT
+var dashVector := Vector2.RIGHT
+var inDash := false
 
-func _physics_process(_delta: float) -> void:
-	
-	var newInputDirection = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+func getDashVelocity(): #set moving to dash direction, move to end of dash vector fast
+	moveDirection = Vector2.RIGHT.rotated(dashVector.angle())
+	targetDirection = moveDirection
+	inDash = true
+	dash_timer.start()
+	return moveDirection * 1000
+
+func dashVisualizer():
+	var mousePos := get_local_mouse_position()
+	var dashAngle := mousePos.angle()
+	dashVector = Vector2.RIGHT.rotated(dashAngle)
+	temp_dash_visual.set_point_position(1, dashVector*(DashSpeed/10))
+
+func calculateMoveVector(newInputDirection: Vector2):
 	if newInputDirection != Vector2.ZERO:
 		targetDirection = newInputDirection.normalized()
 
@@ -24,9 +38,23 @@ func _physics_process(_delta: float) -> void:
 	var current_angle := moveDirection.angle()
 	turn_speed = clamp(turn_speed_divider/SPEED,0,1)
 	var smooth_angle := lerp_angle(current_angle, target_angle, turn_speed)
-	moveDirection = Vector2.RIGHT.rotated(smooth_angle)
+	return Vector2.RIGHT.rotated(smooth_angle)
 
-	if DEBUG_GRAPHICS:
+func _on_dash_timer_timeout() -> void:
+	dash_cooldown.start()
+	inDash = false
+
+func _physics_process(_delta: float) -> void:
+	
+	var newInputDirection = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	if not inDash:
+		moveDirection = calculateMoveVector(newInputDirection)
+
+	if %GameManager.DEBUG_MODE:
 		input_vector.set_point_position(1, newInputDirection.normalized()*25)
 		current_vector.set_point_position(1, moveDirection*25)
 	
@@ -34,5 +62,12 @@ func _physics_process(_delta: float) -> void:
 		FreezeFrameManager.freezeFrame(0.5)
 	
 	velocity = moveDirection * SPEED
+	
+	if Input.is_action_pressed("dash") and dash_cooldown.is_stopped():
+		dashVisualizer()
+	if Input.is_action_just_released("dash") and dash_cooldown.is_stopped():
+		velocity = getDashVelocity()
+	if inDash:
+		velocity = moveDirection * DashSpeed
 	
 	move_and_slide()
